49b7a67d3d90facadd5b95698f33fe1c08ca2c87
Jasper Abarquez <78329142+KuyaJasper@users.noreply.github.com>
2021-04-17 17:16:36 -0400

    set up data

---
+++ index.js
@@ -0,0 +1,7 @@
+const Manager = require('./Data/manager');
+const Engineer = require('./Data/engineer');
+const Intern = require('./Data/intern');
+const generateHTML = require('./Data/generateHTML');
+const inquirer = require('inquirer');
+const fs = require('fs');
+const util = require('util');
